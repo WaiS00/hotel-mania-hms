83c7e7c91f4dc11ee7d8c72a79bd20a874b86f63
WaiS <waisiong144@gmail.com>
2022-07-22 14:40:04 +0800

    first edition of header

--- index.php
+++ index.php
@@ -13,7 +13,24 @@
 </head>
 
 <body>
-    
+<nav class="navbar nav1 navbar-expand-lg py-3 fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="resources/hm_logo.png" alt="" width="100" height="100" class="d-inline-block align-text-mid">
+      Hotel Mania
+    </a>
+    <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <li class="hvr-float-shadow">
+                <a href="#" class="nav-link"><span class="link-text">Sign Up</span></a>
+            </li>
+            <li class="hvr-float-shadow">
+                <a href="#" class="nav-link"><span class="link-text">Log In</span></a>
+            </li>
+        </li>
+    </ul>
+  </div>
+</nav>
 
     <!-- Bootstrap JS CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
