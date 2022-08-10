e8e3f91a526a221f8e348940c9c56650847c5655
WaiS <waisiong144@gmail.com>
2022-08-11 01:28:09 +0800

    first index.php

--- index.php
+++ index.php
@@ -18,7 +18,81 @@
 
 <?php include 'include_php/header.php';?>
 
-<br><br><br><br><br><br>
+<body>
+    <section id="carousel-picture">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="resources/hm_homepage1.png" class="d-block w-100 carouselimg" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5> Hotel Mania</h5>
+                    <p> “The Most Cheap-pluck HMS Award”</p>
+                </div>
+            </div>
+        <div class="carousel-item">
+            <img src="resources/hm_homepage2.png" class="d-block w-100 carouselimg" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5> Hotel Mania</h5>
+                    <p> “The Most Cheap-pluck HMS Award”</p>
+                </div>
+        </div>
+        <div class="carousel-item">
+            <img src="resources/hm_homepage1.png" class="d-block w-100 carouselimg" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5> Hotel Mania</h5>
+                    <p> “The Most Cheap-pluck HMS Award”</p>
+                </div>
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+    </section>
+
+    <section id="new-release">
+    <div class="container new">
+        <i href="#" class="bi bi-newspaper new-release-cam"> New Facilities !</i>
+    </div>
+        <div class="card mb-3 shadow-lg p-3 mb-5 bg-white rounded">
+        <div class="row g-0 row-center">
+            <div class="col-md-4">
+            <img src="resources/facilities-swimming.png" class="img-fluid rounded-start dslr-image" alt="...">
+            </div>
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-6 justify-content-center align-self-center">
+            <div class="card-body">
+                <div class="dslr-description ">
+                    <h2 class="card-title">Olympic-Size Swimming Pool </h2>
+                    <p class="card-text text-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p class="card-text"><small class="text-muted">While stock last hehe</small></p>
+                    <?php if($_SESSION['type'] != 'admin'){
+                        ?>
+                    <a href="product-list.php" class="btn view-btn" role="button" style="color:white;">Click here to view more!</a>
+                    <?php 
+                        }else{
+                    ?>
+                        <p>Logout or Login to customer account to view more</p>
+                        <?php }?>
+                        
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
 
 
 <?php include 'include_php/footer.php';?>
