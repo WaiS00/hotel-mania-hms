9f72b5df9cbba75872186ee830d437ff687c309d
WaiS <waisiong144@gmail.com>
2022-08-10 15:22:59 +0800

    header completed

--- include_php/header.php
+++ include_php/header.php
@@ -1,5 +1,5 @@
 <?php session_start();?>
-<link rel="stylesheet" href="/css/homepage.css">
+<link rel="stylesheet" href="/css/header.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
