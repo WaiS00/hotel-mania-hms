186770623c3de7b3bec5a4645fade83bfe0bdf1d
WaiS <waisiong144@gmail.com>
2022-08-10 15:02:06 +0800

    added empty resources (homepage)

--- index.php
+++ index.php
@@ -5,32 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <!-- Bootstrap Icons CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
     <title>Hotel Mania</title>
+    <link rel="icon" href="resources/hm_logo.png"/>
 </head>
 
 <body>
-<nav class="navbar nav1 navbar-expand-lg py-3 fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="resources/hm_logo.png" alt="" width="100" height="100" class="d-inline-block align-text-mid">
-      Hotel Mania
-    </a>
-    <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-            <li class="hvr-float-shadow">
-                <a href="#" class="nav-link"><span class="link-text">Sign Up</span></a>
-            </li>
-            <li class="hvr-float-shadow">
-                <a href="#" class="nav-link"><span class="link-text">Log In</span></a>
-            </li>
-        </li>
-    </ul>
-  </div>
-</nav>
+
+<?php include 'include_php/header.php';?>
+
+
+
+
+<?php include 'include_php/footer.php';?>
 
     <!-- Bootstrap JS CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
