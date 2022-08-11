6c89b701afb3298bb93accecd0b802d5aebda12d
WaiS <waisiong144@gmail.com>
2022-08-11 23:06:04 +0800

    completed first version of about us page

---
+++ about_us.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+    <title>Hotel Mania</title>
+    <link rel="stylesheet" href="css/about_us.css">
+    <link rel="icon" href="resources/hm_logo.png"/>
+</head>
+
+<body>
+<?php include 'include_php/header.php';?>
+
+<div class="container contactus">
+    <div class="row align-items-center">
+        <div class="col-7">
+            <div class="map-responsive">
+                <iframe class="responsive-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15935.189745264857!2d101.69651978199569!3d3.14808051314377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc482b1dcd5813%3A0x106058ce3bc33aba!2sMenara%20Kuala%20Lumpur!5e0!3m2!1sen!2smy!4v1636091856376!5m2!1sen!2smy" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+        <div class="col-5">
+            <h2>Hotel Mania <br> is located at the heart of the city, <br> Kuala Lumpur. </h2>
+        <br>
+        </div>
+    </div>
+</div>
+
+<?php include 'include_php/footer.php';?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+
+</html>
