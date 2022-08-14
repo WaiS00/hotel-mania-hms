2a52a4c70f04ac1a079990aa469c13e8e85122db
WaiS <waisiong144@gmail.com>
2022-08-15 01:36:23 +0800

    created product_db and queries

---
+++ backend/product_db.php
@@ -0,0 +1,39 @@
+<?php
+require_once "DBController.php";
+
+class product_db extends DBController{
+
+    function getAllProduct()
+    {
+        $query = "SELECT * FROM roomdb";
+        
+        $productResult = $this->getDBResult($query);
+        return $productResult;
+    }
+
+    function getNormal()
+    {
+        $query = "SELECT * FROM roomdb WHERE roomType='Normal'";
+        
+        $productResult = $this->getDBResult($query);
+        return $productResult;
+    }
+
+    function getDeluxe()
+    {
+        $query = "SELECT * FROM roomdb WHERE roomType='Deluxe'";
+        
+        $productResult = $this->getDBResult($query);
+        return $productResult;
+    }
+
+    function getExecutive()
+    {
+        $query = "SELECT * FROM roomdb WHERE roomType='Executive'";
+        
+        $productResult = $this->getDBResult($query);
+        return $productResult;
+    }
+}
+
+?>
