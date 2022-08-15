d01e71abc63777b2cb2b3231fbb821354a9c62ee
WaiS <waisiong144@gmail.com>
2022-08-15 17:29:44 +0800

    added foreign key constraint into the coding

--- backend/register_backend.php
+++ backend/register_backend.php
@@ -13,6 +13,7 @@
 	$fullName   = sanitise($pdo,$_POST['full_name']);
 	$ic   = sanitise($pdo,$_POST['ic_num']);
   	$tbl_name = 'userdb';
+  	$tbl_name1 = 'customerdb';
 
 	$validation = data_validation($_POST['username'], "/^[a-z\d_]{5,20}$/" , "username");
   	$validation .= data_validation($_POST['pwd'], '/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6,12}$/', "password- at least one letter, at least one number, and there have to be 6-12 characters");
@@ -20,10 +21,20 @@
   	$validation .= data_validation($_POST['full_name'], "/.+/", "full Name");
 
 		if($validation==""){
-			$query = "INSERT INTO $tbl_name (userid, fullName, telno,address, email, login, pass, icNum) 
-			VALUES(NULL,$fullName, $telno, $address, $email, $myusername, '$mypassword', '$ic')";
-		
+			$query = "INSERT INTO $tbl_name (userid, fullName, telno,address, email, login, pass, userType) 
+			VALUES(NULL,$fullName, $telno, $address, $email, $myusername, '$mypassword', 'customer')";
+			
 			$result = $pdo->query($query);
+
+			$query3 = "SELECT userid FROM userdb WHERE email=$email";
+			$result2 = $pdo->query($query3);
+			$result2 = $result2->fetch(PDO::FETCH_ASSOC);
+			
+			$userID = $result2['userid'];
+
+			$query2 = "INSERT INTO $tbl_name1 (customerId, icNumber, userid) 
+			VALUES(NULL, $ic, $userID)";
+			$result = $pdo->query($query2);
 
 			if (! $result){
 				die('Error: ');
