1e3478d3513ce6f510f7f1bba35cad38c71a0052
WaiS <waisiong144@gmail.com>
2022-08-15 17:30:29 +0800

    first version of database for hotel mania

--- backend/hotel_mania Database.sql
+++ backend/hotel_mania Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 13, 2022 at 05:10 PM
+-- Generation Time: Aug 15, 2022 at 09:30 AM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.11
 
@@ -51,6 +51,25 @@ CREATE TABLE `bookingdb` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customerdb`
+--
+
+CREATE TABLE `customerdb` (
+  `customerId` int(11) NOT NULL,
+  `icNumber` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `customerdb`
+--
+
+INSERT INTO `customerdb` (`customerId`, `icNumber`, `userId`) VALUES
+(1, 108081312, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `roomdb`
 --
 
@@ -61,8 +80,18 @@ CREATE TABLE `roomdb` (
   `roomDetails` varchar(255) NOT NULL,
   `roomFloor` int(11) NOT NULL,
   `roomAvailability` enum('available','unavailable') NOT NULL,
-  `roomNumber` int(11) NOT NULL
+  `roomNumber` int(11) NOT NULL,
+  `roomImage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `roomdb`
+--
+
+INSERT INTO `roomdb` (`roomId`, `roomType`, `roomRate`, `roomDetails`, `roomFloor`, `roomAvailability`, `roomNumber`, `roomImage`) VALUES
+(1, 'Normal', 100, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 1, 'available', 1, 'resources/normal-room.png'),
+(2, 'Deluxe', 200, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 2, 'available', 1, 'resources/deluxe-room.png'),
+(3, 'Executive', 300, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 3, 'available', 1, 'resources/executive-room.png');
 
 -- --------------------------------------------------------
 
@@ -79,8 +108,15 @@ CREATE TABLE `userdb` (
   `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `login` varchar(20) NOT NULL,
   `pass` varchar(255) NOT NULL,
-  `icNum` int(11) NOT NULL
+  `userType` enum('worker','customer') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='user database';
+
+--
+-- Dumping data for table `userdb`
+--
+
+INSERT INTO `userdb` (`userid`, `fullName`, `telno`, `country`, `address`, `email`, `login`, `pass`, `userType`) VALUES
+(2, 'Chin Wai Siong', '0122978732', NULL, 'No 15, Jalan Desa Bukit Tiara 3, Desa Bukit Tiara, Cheras 56000 Kuala Lumpur', 'chinwaisiong@hotmail.com', 'kok123', '$2y$10$LfzL.gH2orFtXFX6zx/IRuAjmov/zOnro5fE6GEg7LVqfEUzZC4vm', 'customer');
 
 -- --------------------------------------------------------
 
@@ -90,13 +126,9 @@ CREATE TABLE `userdb` (
 
 CREATE TABLE `workerdb` (
   `workerId` int(11) NOT NULL,
-  `login` varchar(20) NOT NULL,
-  `pass` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `fullName` varchar(100) NOT NULL,
-  `country` varchar(100) NOT NULL,
   `jobStatus` enum('Full Time','Part Time','Intern') NOT NULL,
-  `workPosition` enum('Cleaning Workers','Managers','Front Desk Workers') NOT NULL
+  `workPosition` enum('Cleaning Workers','Managers','Front Desk Workers') NOT NULL,
+  `userId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -116,16 +148,31 @@ ALTER TABLE `bookingdb`
   ADD PRIMARY KEY (`bookingId`);
 
 --
+-- Indexes for table `customerdb`
+--
+ALTER TABLE `customerdb`
+  ADD PRIMARY KEY (`customerId`),
+  ADD KEY `test1` (`userId`);
+
+--
 -- Indexes for table `roomdb`
 --
 ALTER TABLE `roomdb`
   ADD PRIMARY KEY (`roomId`);
 
 --
+-- Indexes for table `userdb`
+--
+ALTER TABLE `userdb`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- Indexes for table `workerdb`
 --
 ALTER TABLE `workerdb`
-  ADD PRIMARY KEY (`workerId`);
+  ADD PRIMARY KEY (`workerId`),
+  ADD KEY `test` (`userId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -144,16 +191,44 @@ ALTER TABLE `bookingdb`
   MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `customerdb`
+--
+ALTER TABLE `customerdb`
+  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `roomdb`
 --
 ALTER TABLE `roomdb`
-  MODIFY `roomId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `roomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `userdb`
+--
+ALTER TABLE `userdb`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `workerdb`
 --
 ALTER TABLE `workerdb`
   MODIFY `workerId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customerdb`
+--
+ALTER TABLE `customerdb`
+  ADD CONSTRAINT `test1` FOREIGN KEY (`userId`) REFERENCES `userdb` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `workerdb`
+--
+ALTER TABLE `workerdb`
+  ADD CONSTRAINT `test` FOREIGN KEY (`userId`) REFERENCES `userdb` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
