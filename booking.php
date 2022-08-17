224d0855e29a52c666463eee4d1bb7edf55c91ca
WaiS <waisiong144@gmail.com>
2022-08-18 01:50:32 +0800

    calendar selection completed

--- booking.php
+++ booking.php
@@ -13,6 +13,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <!-- Bootstrap Icons CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <title>Hotel Mania</title>
     <link rel="icon" href="resources/hm_logo.png"/>
 </head>
@@ -32,8 +37,8 @@
 			<tr>
 				<td><label>Room Type</label></td>
 				<td>
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Select Room Type</option>
+                <select class="form-select" aria-label="Default select example" required>
+                    <option value="">Select Room Type </option>
                     <?php
                     if (!empty($product_array)) {
                         foreach ($product_array as $key => $value) {
@@ -41,17 +46,27 @@
                     <option value="<?php echo $product_array[$key]["roomType"]; ?>"><?php echo $product_array[$key]["roomType"]; ?></option>
                     <?php }}?>
                 </select>
-			</tr>																																		
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Submit" class="btn btn-primary submitbtn"></td>
+			</tr>  
+            <tr>
+				<td><label>Date Time</label></td>
+				<td>
+                    <div class="input-group date" id="datepicker">
+                        <input type="text" class="form-control">
+                            <div class="input-group-append">
+                                <div class="input-group-text bg-white d-block">
+                                    <i class="fa fa-calendar"></i>
+                                </div>
+                            </div>
+                    </div>
+                </td>
 			</tr>
+																																		
 		</table>		
 	</form>
 <?php include 'include_php/footer.php';?>
 
     <!-- Bootstrap JS CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="js/booking.js"> </script>
 </body>
 
 </html>
