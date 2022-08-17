eca30403cfb470c89a8d39f3a1fa533876874de6
WaiS <waisiong144@gmail.com>
2022-08-18 03:07:48 +0800

    updated with AJAX JQUERY that can change difference without reloading

--- js/booking.js
+++ js/booking.js
@@ -6,6 +6,15 @@ $(function() {
 
 $(function() {
     $ ( '#datepicker2' ) . datepicker ( {
-        format: 'yyyy-mm-dd'
-    } ) ;
+        format: 'yyyy-mm-dd',
+    } ).on("changeDate", function (e){
+        var start= $("#datepicker").datepicker("getDate");
+        var end= $("#datepicker2").datepicker("getDate");
+        days = (end- start) / (1000 * 60 * 60 * 24);
+        document.getElementById("ans").innerHTML = days;
+    }) ;
 });
+
+
+
+
