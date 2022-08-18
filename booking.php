19f18cb2c18a088c02dc25c737062a2f3b331a5f
WaiS <waisiong144@gmail.com>
2022-08-18 23:13:18 +0800

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,5 +1,6 @@
-<?php require_once 'backend/SQL_login.php';?>
+<?php session_start();?>
 <?php require_once "backend/config.php"; ?>
+<?php require_once 'backend/booking_backend.php';?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,8 +20,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-    <script type="text/javascript" src="js/booking.js"> </script>
     <title>Hotel Mania</title>
+    <script type="text/javascript" src="js/booking.js"> </script>
     <link rel="icon" href="resources/hm_logo.png"/>
 </head>
 
@@ -39,21 +40,21 @@
 			<tr>
 				<td><label>Room Type</label></td>
 				<td>
-                <select class="form-select" id="ddlselect" required>
+                <select id="ddlselect" name="ddlselect" required>
                     <option value="" selected disabled>Select Room Type </option>
                     <?php
                     if (!empty($product_array)) {
                         foreach ($product_array as $key => $value) {
-                    ?> 
+                    ?>
                     <option value="<?php echo $product_array[$key]["roomType"]; ?>"><?php echo $product_array[$key]["roomType"]; ?></option>
                     <?php }}?>
                 </select>
-			</tr>  
+			</tr>
             <tr>
 				<td><label>Checkin Date</label></td>
 				<td>
                     <div class="input-group date" id="datepicker">
-                        <input type="text" class="form-control" id="date1" required>
+                        <input type="text" class="form-control" id="date1" name="date3" required></input>
                             <div class="input-group-append">
                                 <div class="input-group-text bg-white d-block">
                                     <i class="fa fa-calendar"></i>
@@ -66,7 +67,7 @@
                 <td><label>Checkout Date</label></td>
 				<td>
                     <div class="input-group date" id="datepicker2">
-                        <input type="text" class="form-control" id="date2" required>
+                        <input type="text" class="form-control" id="date2" name="date4" required></input>
                             <div class="input-group-append">
                                 <div class="input-group-text bg-white d-block">
                                     <i class="fa fa-calendar"></i>
@@ -85,9 +86,12 @@
                 <td><label>Room Type Selected:</label>
                 <td><h4 style="display: inline-block" id="txtresults">None </h4>
             </tr>
-
+            <tr>
+				<td><input type="submit" value="Submit" name="submit" class="btn btn-primary submitbtn"></td>
+			</tr>
 		</table>		
 	</form>
+
 <?php include 'include_php/footer.php';?>
 
 </body>
