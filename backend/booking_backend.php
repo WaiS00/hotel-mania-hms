ab4be5c763d899d06b807ef92e4e7143982244e4
WaiS <waisiong144@gmail.com>
2022-08-19 00:02:37 +0800

    backend to post values into bookingcart database

---
+++ backend/booking_backend.php
@@ -0,0 +1,27 @@
+<?php 
+
+  require_once 'SQL_login.php';
+  
+  if(isset($_POST['submit'])){
+    if(isset($_SESSION['fullName'])){
+
+            $select = $_POST['ddlselect'];
+            $checkInDate = $_POST['date3'];
+            $checkoutdate = $_POST['date4'];
+            $tbl_name = 'bookingcartdb';
+
+                    $query = "INSERT INTO $tbl_name (bookingcartId, roomType, checkInDate, checkoutDate) 
+                    VALUES(NULL,'$select', '$checkInDate', '$checkoutdate')";
+                    
+                    $result = $pdo->query($query);
+
+                    echo "<script type='text/javascript'>window.location.href = './payment.php';</script>";
+                
+            }else{
+                echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
+                echo "<script> location.href = './login.php';</script>";
+            }
+    }
+    
+
+?>
