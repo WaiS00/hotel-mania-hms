8a44dcbec32cd20c82dae73636d23fea3e1fd5f9
WaiS <waisiong144@gmail.com>
2022-08-21 03:23:44 +0800

    format for calculating days and inserting to database

--- backend/booking_backend.php
+++ backend/booking_backend.php
@@ -10,13 +10,18 @@
             $checkoutdate = $_POST['date4'];
             $tbl_name = 'bookingcartdb';
 
-                    $query = "INSERT INTO $tbl_name (bookingcartId, roomType, checkInDate, checkoutDate) 
-                    VALUES(NULL,'$select', '$checkInDate', '$checkoutdate')";
+            $dayDiff = abs(strtotime($checkoutdate) - strtotime($checkInDate));
+            $years = floor($dayDiff / (365*60*60*24));
+            $months = floor(($dayDiff - $years * 365*60*60*24) / (30*60*60*24));
+            $days = floor(($dayDiff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+
+                    $query = "INSERT INTO $tbl_name (bookingcartId, roomType, checkInDate, checkoutDate, dayDiff) 
+                    VALUES(NULL,'$select', '$checkInDate', '$checkoutdate', '$days')";
                     
                     $result = $pdo->query($query);
 
                     echo "<script type='text/javascript'>window.location.href = './payment.php';</script>";
-                
+
             }else{
                 echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
                 echo "<script> location.href = './login.php';</script>";
