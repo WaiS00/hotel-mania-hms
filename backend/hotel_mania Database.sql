c8707283cd457e37cf8abb7b864c00b355ebef53
WaiS <waisiong144@gmail.com>
2022-08-22 00:09:03 +0800

    Update hotel_mania Database.sql

--- backend/hotel_mania Database.sql
+++ backend/hotel_mania Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 21, 2022 at 04:06 PM
+-- Generation Time: Aug 21, 2022 at 04:08 PM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.11
 
@@ -21,6 +21,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `hotel_mania`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendancedb`
+--
+
+CREATE TABLE `attendancedb` (
+  `attendanceId` int(11) NOT NULL,
+  `clockinDateTime` varchar(144) NOT NULL,
+  `clockoutDateTime` varchar(144) NOT NULL,
+  `workerId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -44,9 +57,117 @@ INSERT INTO `bookingcartdb` (`bookingcartId`, `roomType`, `checkInDate`, `checko
 (12, 'Normal', '2022-08-29', '2022-08-31', '2'),
 (48, 'Normal', '2022-08-01', '2022-08-30', '29');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookingdb`
+--
+
+CREATE TABLE `bookingdb` (
+  `bookingId` int(11) NOT NULL,
+  `checkInDate` date NOT NULL,
+  `roomType` enum('Normal','Deluxe','Executive') NOT NULL,
+  `checkOutDate` date NOT NULL,
+  `bookingTotalPrice` double NOT NULL,
+  `numberOfGuest` int(11) NOT NULL,
+  `customerId` int(11) NOT NULL,
+  `paymentStatus` enum('paid','unpaid') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customerdb`
+--
+
+CREATE TABLE `customerdb` (
+  `customerId` int(11) NOT NULL,
+  `icNumber` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `customerdb`
+--
+
+INSERT INTO `customerdb` (`customerId`, `icNumber`, `userId`) VALUES
+(1, 108081312, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roomdb`
+--
+
+CREATE TABLE `roomdb` (
+  `roomId` int(11) NOT NULL,
+  `roomType` enum('Normal','Deluxe','Executive') NOT NULL,
+  `roomRate` double NOT NULL,
+  `roomDetails` varchar(255) NOT NULL,
+  `roomFloor` int(11) NOT NULL,
+  `roomAvailability` enum('available','unavailable') NOT NULL,
+  `roomNumber` int(11) NOT NULL,
+  `roomImage` text NOT NULL,
+  `customerId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `roomdb`
+--
+
+INSERT INTO `roomdb` (`roomId`, `roomType`, `roomRate`, `roomDetails`, `roomFloor`, `roomAvailability`, `roomNumber`, `roomImage`, `customerId`) VALUES
+(1, 'Normal', 100, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 1, 'available', 1, 'resources/normal-room.png', 0),
+(2, 'Deluxe', 200, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 2, 'available', 1, 'resources/deluxe-room.png', 0),
+(3, 'Executive', 300, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 3, 'available', 1, 'resources/executive-room.png', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdb`
+--
+
+CREATE TABLE `userdb` (
+  `userid` int(11) NOT NULL,
+  `fullName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `telno` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `country` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `login` varchar(20) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `userType` enum('worker','customer') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='user database';
+
+--
+-- Dumping data for table `userdb`
+--
+
+INSERT INTO `userdb` (`userid`, `fullName`, `telno`, `country`, `address`, `email`, `login`, `pass`, `userType`) VALUES
+(2, 'Chin Wai Siong', '0122978732', NULL, 'No 15, Jalan Desa Bukit Tiara 3, Desa Bukit Tiara, Cheras 56000 Kuala Lumpur', 'chinwaisiong@hotmail.com', 'kok123', '$2y$10$LfzL.gH2orFtXFX6zx/IRuAjmov/zOnro5fE6GEg7LVqfEUzZC4vm', 'customer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workerdb`
+--
+
+CREATE TABLE `workerdb` (
+  `workerId` int(11) NOT NULL,
+  `jobStatus` enum('Full Time','Part Time','Intern') NOT NULL,
+  `workPosition` enum('Cleaning Workers','Managers','Front Desk Workers') NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendancedb`
+--
+ALTER TABLE `attendancedb`
+  ADD PRIMARY KEY (`attendanceId`),
+  ADD KEY `test3` (`workerId`);
 
 --
 -- Indexes for table `bookingcartdb`
@@ -55,14 +176,119 @@ ALTER TABLE `bookingcartdb`
   ADD PRIMARY KEY (`bookingcartId`);
 
 --
+-- Indexes for table `bookingdb`
+--
+ALTER TABLE `bookingdb`
+  ADD PRIMARY KEY (`bookingId`),
+  ADD KEY `test2` (`customerId`);
+
+--
+-- Indexes for table `customerdb`
+--
+ALTER TABLE `customerdb`
+  ADD PRIMARY KEY (`customerId`),
+  ADD KEY `test1` (`userId`);
+
+--
+-- Indexes for table `roomdb`
+--
+ALTER TABLE `roomdb`
+  ADD PRIMARY KEY (`roomId`),
+  ADD KEY `test4` (`customerId`);
+
+--
+-- Indexes for table `userdb`
+--
+ALTER TABLE `userdb`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `workerdb`
+--
+ALTER TABLE `workerdb`
+  ADD PRIMARY KEY (`workerId`),
+  ADD KEY `test` (`userId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `attendancedb`
+--
+ALTER TABLE `attendancedb`
+  MODIFY `attendanceId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `bookingcartdb`
 --
 ALTER TABLE `bookingcartdb`
   MODIFY `bookingcartId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- AUTO_INCREMENT for table `bookingdb`
+--
+ALTER TABLE `bookingdb`
+  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customerdb`
+--
+ALTER TABLE `customerdb`
+  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `roomdb`
+--
+ALTER TABLE `roomdb`
+  MODIFY `roomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `userdb`
+--
+ALTER TABLE `userdb`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `workerdb`
+--
+ALTER TABLE `workerdb`
+  MODIFY `workerId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendancedb`
+--
+ALTER TABLE `attendancedb`
+  ADD CONSTRAINT `test3` FOREIGN KEY (`workerId`) REFERENCES `workerdb` (`workerId`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `bookingdb`
+--
+ALTER TABLE `bookingdb`
+  ADD CONSTRAINT `test2` FOREIGN KEY (`customerId`) REFERENCES `customerdb` (`customerId`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `customerdb`
+--
+ALTER TABLE `customerdb`
+  ADD CONSTRAINT `test1` FOREIGN KEY (`userId`) REFERENCES `userdb` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `roomdb`
+--
+ALTER TABLE `roomdb`
+  ADD CONSTRAINT `test4` FOREIGN KEY (`customerId`) REFERENCES `customerdb` (`customerId`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `workerdb`
+--
+ALTER TABLE `workerdb`
+  ADD CONSTRAINT `test` FOREIGN KEY (`userId`) REFERENCES `userdb` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
