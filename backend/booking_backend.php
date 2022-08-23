6421806ac2380fd70230ce644c1de2a496a2d031
WaiS <waisiong144@gmail.com>
2022-08-23 23:29:07 +0800

    isset session userId = userId from customerDb, the customerid will be inserted

--- backend/booking_backend.php
+++ backend/booking_backend.php
@@ -3,7 +3,7 @@
   require_once 'SQL_login.php';
   
   if(isset($_POST['submit'])){
-    if(isset($_SESSION['fullName'])){
+    if(isset($_SESSION['userId'])){
 
             $select = $_POST['ddlselect'];
             $checkInDate = $_POST['date3'];
@@ -15,12 +15,18 @@
             $months = floor(($dayDiff - $years * 365*60*60*24) / (30*60*60*24));
             $days = floor(($dayDiff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
 
-                    $query = "INSERT INTO $tbl_name (bookingcartId, roomType, checkInDate, checkoutDate, dayDiff) 
-                    VALUES(NULL,'$select', '$checkInDate', '$checkoutdate', '$days')";
-                    
-                    $result = $pdo->query($query);
+                $query3 = "SELECT customerid FROM customerdb WHERE userId = '{$_SESSION['userId']}' ";
+                $result2 = $pdo->query($query3);
+                $result2 = $result2->fetch(PDO::FETCH_ASSOC);
+                
+                $customerid = $result2['customerid'];
 
-                    echo "<script type='text/javascript'>window.location.href = './payment.php';</script>";
+                $query = "INSERT INTO $tbl_name (bookingcartId, roomType, checkInDate, checkoutDate, dayDiff, customerid) 
+                VALUES(NULL,'$select', '$checkInDate', '$checkoutdate', '$days', $customerid)";
+                
+                $result = $pdo->query($query);
+
+                echo "<script type='text/javascript'>window.location.href = './payment.php';</script>";
 
             }else{
                 echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
