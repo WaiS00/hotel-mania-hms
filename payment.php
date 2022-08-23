1322c13055ed17d1a40ca774edf7d22e720fcdaa
WaiS <waisiong144@gmail.com>
2022-08-23 23:29:31 +0800

    get data from database

--- payment.php
+++ payment.php
@@ -13,7 +13,7 @@
     <title>Hotel Mania</title>
     <link rel="icon" href="resources/hm_logo.png"/>
 </head>
-
+<?php require_once "backend/config.php"; ?>
 <?php include 'include_php/header.php';?>
 
 <body>
@@ -69,15 +69,18 @@
       <h4>Cart
         <span class="price" style="color:black">
           <i class="fa fa-shopping-cart"></i>
-          <b>4</b>
         </span>
       </h4>
-      <p><a href="#">Product 1</a> <span class="price">$15</span></p>
-      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+      <?php
+        $query = "SELECT * FROM bookingcartdb b, customerdb c  WHERE b.customerid=c.customerid";
+        $product_array = $product_db->cartItem($query);
+        if (!empty($product_array)) {
+            foreach ($product_array as $key => $value) {
+        ?> 
+      <p><a href="#">Room Type</a> <span class="price"><?php echo $product_array[$key]["roomType"]; ?></span></p>
       <hr>
-      <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+      <p>Total <span id="price" name="price" class="price" style="color:black"><b>$30</b></span></p>
+      <?php }} ?>
     </div>
   </div>
 </div>
