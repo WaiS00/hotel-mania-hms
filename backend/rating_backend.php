0287c4316cfa0484b38add2974ffa13c0a756b58
WaiS <waisiong144@gmail.com>
2022-09-04 01:14:48 +0800

    queries to fetch and post values into new ratingdb

---
+++ backend/rating_backend.php
@@ -0,0 +1,31 @@
+<?php 
+
+  require_once 'SQL_login.php';
+  if(isset($_POST['submit'])){
+    if(isset($_SESSION['userId'])){
+
+            $rate = $_POST['rate'];
+            $review = $_POST['review'];
+            $tbl_name = 'ratingdb';
+
+            $query3 = "SELECT email FROM userdb WHERE userId = '{$_SESSION['userId']}' ";
+            $result2 = $pdo->query($query3);
+            $result2 = $result2->fetch(PDO::FETCH_ASSOC);
+            
+            $email = $result2['email'];
+
+            $query = "INSERT INTO $tbl_name (ratingId, rate, review, email) 
+            VALUES(NULL, '$rate', '$review', '$email')";
+                
+                $result = $pdo->query($query);
+                echo "<script type='text/javascript'>alert('Review Submitted Successfully.');</script>";
+                echo "<script type='text/javascript'>window.location.href = './rating_main.php';</script>";
+
+    }else{
+        echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
+        echo "<script> location.href = './login.php';</script>";
+    }
+    
+  }    
+
+?>
