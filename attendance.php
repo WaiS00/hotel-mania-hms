22d3824b9f716e0ccd348dee146ef90a031b0365
WaiS <waisiong144@gmail.com>
2022-09-06 00:03:27 +0800

    clock-in successful (first ver) - will be updated

--- attendance.php
+++ attendance.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once "backend/attendance_backend.php"; ?>
@@ -20,21 +21,24 @@
 <body>
 
 <h4>Attendance (clock-in/clock-out)</h4>
+<form method="post">
   <div class="row justify-content-md-center">
     <div class="col col-lg-2">
-        <input id="Button" type="clockin" value="Clock-in" name="clockin" class="btn btn-primary submitbtn">
+        <input type="submit" id="Button" name="submit" value="Clock-in" class="btn btn-primary submitbtn" readonly>
     </div>
     <div class="col col-lg-2">
     </div>
     <div class="col col-lg-2">
-        <input id="Button" type="clockout" value="Clock-out" name="clockout" class="btn btn-primary submitbtn">
+        <input type="submit" id="Button" name="submit" value="Clock-out" class="btn btn-primary submitbtn" readonly>
     </div>
   </div>
+</form>
     <table style="margin-left: auto; margin-right: auto; border: 1px solid;border-collapse: collapse; width: 50%;">
         <tr>
             <td style="border: 1px solid;text-align: center;"><b> Clock-in Date Time <b> </td>
             <td style="border: 1px solid;text-align: center;"><b> Clock-out Date Time <b> </td>
-        </tr>              
+            <td style="border: 1px solid;text-align: center;"><b> Full Name<b> </td>
+        </tr>
     </table>
 
 <?php include 'include_php/footer.php';?>
---
+++ backend/attendance_backend.php
@@ -0,0 +1,33 @@
+<?php 
+
+require_once 'SQL_login.php';
+require_once "config.php"; 
+
+    if($_POST['submit'] == 'Clock-in'){
+        if(isset($_SESSION['userId'])){
+            $date = date('Y-m-d H:i:s');
+            $tbl_name = 'attendancedb';
+
+            $query3 = "SELECT * FROM userdb WHERE userId = '{$_SESSION['userId']}' ";
+            $result2 = $pdo->query($query3);
+            $result2 = $result2->fetch(PDO::FETCH_ASSOC);
+            
+            $fullName = $result2['fullName'];
+
+            $query1 = "INSERT INTO $tbl_name (attendanceId, clockinDateTime, clockoutDateTime, fullName) 
+            VALUES(NULL, '$date', NULL, '$fullName')";
+            
+            $result = $pdo->query($query1);
+            echo "<script type='text/javascript'>alert('Clock-in Successfully.');</script>";
+            echo "<script type='text/javascript'>window.location.href = './attendance.php';</script>";
+        }
+        echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
+        echo "<script> location.href = './login.php';</script>";
+    }
+
+    
+    if($_POST['submit'] == 'Clock-out'){
+        
+    }
+
+?>
