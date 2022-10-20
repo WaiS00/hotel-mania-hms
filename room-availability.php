f58f3d94af7350fbac78e9ba56457376f6a9638f
WaiS <waisiong144@gmail.com>
2022-10-21 02:48:52 +0800

    it can only select one checkbox at a time (to prevent ambiguity)

--- room-availability.php
+++ room-availability.php
@@ -38,14 +38,14 @@
                         if($product_array[$key]["roomAvailability"] == "available"){
                             ?> 
                                 <li class="seat">
-                                    <input type="checkbox" id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                                 </li>
                             <?php
                                 }else{
                             ?>
                                 <li class="seat">
-                                    <input type="checkbox" disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                                 </li>
                 
@@ -68,14 +68,14 @@
                         if($product_array[$key]["roomAvailability"] == "available"){
                             ?> 
                                 <li class="seat">
-                                    <input type="checkbox" id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                                 </li>
                             <?php
                                 }else{
                             ?>
                                 <li class="seat">
-                                    <input type="checkbox" disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                                 </li>
                 
@@ -98,14 +98,14 @@
                         if($product_array[$key]["roomAvailability"] == "available"){
             ?> 
                 <li class="seat">
-                    <input type="checkbox" id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                 </li>
             <?php
                 }else{
             ?>
                 <li class="seat">
-                    <input type="checkbox" disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                 </li>
 
@@ -128,14 +128,14 @@
                         if($product_array[$key]["roomAvailability"] == "available"){
                             ?> 
                                 <li class="seat">
-                                    <input type="checkbox" id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>" id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                                 </li>
                             <?php
                                 }else{
                             ?>
                                 <li class="seat">
-                                    <input type="checkbox" disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
+                                    <input type="checkbox" class="checkoption" value="<?php echo $product_array[$key]["roomNumber"]; ?>"disabled id="<?php echo $product_array[$key]["roomNumber"]; ?>"/>
                                     <label for="<?php echo $product_array[$key]["roomId"]; ?>"><?php echo $product_array[$key]["roomNumber"]; ?></label>
                                 </li>
                 
@@ -149,10 +149,60 @@
   </ol>
   <div class="exit exit--front fuselage">
     </div>
+<br>
+    <table class="center">
+			<tr>
+				<td><label>Username</label></td>
+				<td><input type="text" name="username" class="form-control" required />
+			</tr>
+			<tr>
+				<td><label>Password</label></td>
+				<td><input type="password" name="pwd" class="form-control" required />
+			</tr>
+			<tr>
+				<td><label>Re-Enter Password</label></td>
+				<td><input type="password" required  class="form-control" />
+			</tr>
+			<tr>
+				<td><label>Full Name</label></td>
+				<td><input type="text" name= "full_name" class="form-control" required  />
+			</tr> 
+			<tr>
+				<td><label>IC/Passport Number</label></td>
+				<td><input type="text" name= "ic_num" class="form-control" required  />
+			</tr> 
+			<tr>
+				<td><label>Telephone No</label></td>
+				<td><input type="text" name="telno"  class="form-control" required />
+			</tr> 
+				<tr>
+				<td><label>Address</label></td>
+				<td><input type="text" name="address" class="form-control" required  />
+			</tr> 
+			<tr>
+				<td><label>Email</label></td>
+				<td><input type="text" name ="email" class="form-control" required />
+			</tr>																														
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Submit" class="btn btn-primary submitbtn"></td>
+			</tr>
+		</table>
+
   </div>
 
-<?php include 'include_php/footer.php';?>
 
+<?php include 'include_php/footer.php';?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+   <script type="text/javascript">
+   $(document).ready(function(){
+
+      $('.checkoption').click(function() {
+         $('.checkoption').not(this).prop('checked', false);
+      });
+
+   });
+   </script>
     <!-- Bootstrap JS CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
