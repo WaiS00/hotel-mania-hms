80b9b19c932e34b438d1deb667809af49839743c
WaiS <waisiong144@gmail.com>
2022-10-23 00:29:48 +0800

    now checkout requires user to checkin before hand

--- backend/checkout_backend.php
+++ backend/checkout_backend.php
@@ -10,6 +10,18 @@
             $tbl_name = 'roomdb';
             $tbl_name1 = 'customerdb';
             
+            $query4 = "SELECT customerId FROM customerdb WHERE icNumber = $ic ";
+            $result3 = $pdo->query($query4);
+            $result3 = $result3->fetch(PDO::FETCH_ASSOC);
+            
+            $customerid = $result3['customerId'];
+      
+            $sql = "SELECT count(*) AS counter FROM bookingdb WHERE customerId = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$customerid]);
+            $counts = $stmt->fetch();
+            if($counts['counter'] == 1){
+
                 $query = "UPDATE $tbl_name 
                 SET customerId=NULL, roomAvailability='Available', checkInDate=NULL, checkOutDate=NULL, numofGuest=NULL
                 WHERE roomNumber=$roomSelected";
@@ -22,6 +34,9 @@
                 $result1 = $pdo->query($query1);
                 echo "<script type='text/javascript'>alert('Check-out Successfully');</script>";
                 echo "<script type='text/javascript'>window.location.href = './booking-list.php';</script>";
+            }          
+            echo "<script type='text/javascript'>alert('Room check-in has not been found');</script>";
+            echo "<script type='text/javascript'>window.location.href = './room-availability.php';</script>";
 
             }else{
             }
