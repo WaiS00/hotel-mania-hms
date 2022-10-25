3809fb1ce2152f40a0f8c2f01fb0fcb3089155ab
WaiS <waisiong144@gmail.com>
2022-10-26 01:04:59 +0800

    update on comments and documentations

--- backend/rating_backend.php
+++ backend/rating_backend.php
@@ -8,20 +8,24 @@
             $review = $_POST['review'];
             $tbl_name = 'ratingdb';
 
+            // get user email from session login
             $query3 = "SELECT email FROM userdb WHERE userId = '{$_SESSION['userId']}' ";
             $result2 = $pdo->query($query3);
             $result2 = $result2->fetch(PDO::FETCH_ASSOC);
             
             $email = $result2['email'];
 
+            // insert into rating database
             $query = "INSERT INTO $tbl_name (ratingId, rate, review, email) 
             VALUES(NULL, '$rate', '$review', '$email')";
-                
+
+            // review submitted successfully
                 $result = $pdo->query($query);
                 echo "<script type='text/javascript'>alert('Review Submitted Successfully.');</script>";
                 echo "<script type='text/javascript'>window.location.href = './rating_main.php';</script>";
 
     }else{
+      // if user has not login
         echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
         echo "<script> location.href = './login.php';</script>";
     }
