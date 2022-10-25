cda50d38c7a9ba1d6b77c8e2738334e91a1c7e65
WaiS <waisiong144@gmail.com>
2022-10-26 01:05:09 +0800

    update on comments and documentations

--- backend/register_backend.php
+++ backend/register_backend.php
@@ -2,10 +2,13 @@
 
   require_once 'SQL_login.php';
 
+  // if username and password has been filled in and submitted, 
   if(isset($_POST['username']) && isset($_POST['pwd'])){
 	
+	// sanitise the post variables
 	$myusername = sanitise($pdo,$_POST['username']);
 	$mypassword = sanitise($pdo,$_POST['pwd']);
+	//hashing of password
 	$mypassword = password_hash($mypassword, PASSWORD_DEFAULT);
 	$telno      = sanitise($pdo,$_POST['telno']);
 	$address    = sanitise($pdo,$_POST['address']);
@@ -15,23 +18,30 @@
   	$tbl_name = 'userdb';
   	$tbl_name1 = 'customerdb';
 
+	// username must be within 5 to 20 characters
 	$validation = data_validation($_POST['username'], "/^[a-z\d_]{5,20}$/" , "username");
-  	$validation .= data_validation($_POST['pwd'], '/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6,12}$/', "password- at least one letter, at least one number, and there have to be 6-12 characters");
+	// password must have one letter, one digit, and be within 6 to 12 characters
+	$validation .= data_validation($_POST['pwd'], '/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{6,12}$/', "password- at least one letter, at least one number, and there have to be 6-12 characters");
+ 	// must be a valid email (with smtg@smtg.smtg)
  	$validation .= data_validation($_POST['email'],  "/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/" , "email");
-  	$validation .= data_validation($_POST['full_name'], "/.+/", "full Name");
+	// must have space between characters
+	$validation .= data_validation($_POST['full_name'], "/.+/", "full Name");
 
 		if($validation==""){
+			// insert values
 			$query = "INSERT INTO $tbl_name (userid, fullName, telno,address, email, login, pass, userType) 
 			VALUES(NULL,$fullName, $telno, $address, $email, $myusername, '$mypassword', 'customer')";
 			
 			$result = $pdo->query($query);
 
+			// select user id where email = the email from post method
 			$query3 = "SELECT userid FROM userdb WHERE email=$email";
 			$result2 = $pdo->query($query3);
 			$result2 = $result2->fetch(PDO::FETCH_ASSOC);
 			
 			$userID = $result2['userid'];
 
+			// insert values from the previous variables
 			$query2 = "INSERT INTO $tbl_name1 (customerId, icNumber, userid) 
 			VALUES(NULL, $ic, $userID)";
 			$result = $pdo->query($query2);
@@ -39,23 +49,26 @@
 			if (! $result){
 				die('Error: ');
 			}
+			// success message
 			echo "<script type='text/javascript'>alert('Registered Successfully');</script>";
 			echo "<script type='text/javascript'>window.location.href = './login.php';</script>";
 		}
 		else{
+			// echo invalid data for the inputs
 			echo $validation;
 		}
 	  }
 
 
 
-
+	// sanitise method
    function sanitise($pdo, $str)
   {
     $str = htmlentities($str);
     return $pdo->quote($str);
   }	
-  
+
+  // method for calling error for invalid data
   function data_validation($data, $data_pattern, $data_type)
   {
 	  if(preg_match($data_pattern, $data)) {
