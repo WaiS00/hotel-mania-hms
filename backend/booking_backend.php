c6c2cd548d2f2f5d70dd9d1754cffd71e3865ddb
WaiS <waisiong144@gmail.com>
2022-10-26 01:04:28 +0800

    update on comments and documentations

--- backend/booking_backend.php
+++ backend/booking_backend.php
@@ -2,7 +2,9 @@
 
   require_once 'SQL_login.php';
   
+  // if submit button is post/ clicked
   if(isset($_POST['submit'])){
+    // if user id is found (if user has login)
     if(isset($_SESSION['userId'])){
 
             $select = $_POST['ddlselect'];
@@ -10,6 +12,8 @@
             $checkoutdate = $_POST['date4'];
             $tbl_name = 'bookingcartdb';
 
+            // change string to time variable
+            // abstract checkout date with checkindate and calculate days based on the years and months difference 
             $dayDiff = abs(strtotime($checkoutdate) - strtotime($checkInDate));
             $years = floor($dayDiff / (365*60*60*24));
             $months = floor(($dayDiff - $years * 365*60*60*24) / (30*60*60*24));
@@ -21,6 +25,7 @@
                 
                 $customerid = $result2['customerid'];
 
+                // insert into database
                 $query = "INSERT INTO $tbl_name (bookingcartId, roomType, checkInDate, checkoutDate, dayDiff, customerid) 
                 VALUES(NULL,'$select', '$checkInDate', '$checkoutdate', '$days', $customerid)";
                 
@@ -28,6 +33,7 @@
                 echo "<script type='text/javascript'>window.location.href = './payment.php';</script>";
 
             }else{
+                // when userid session is not found
                 echo "<script type='text/javascript'>alert('Please login or register your account.');</script>";
                 echo "<script> location.href = './login.php';</script>";
             }
