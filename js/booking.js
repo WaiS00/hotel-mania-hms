bdd3227e8d999c98874028ce6106ca7cc223a1f8
WaiS <waisiong144@gmail.com>
2022-11-08 00:26:48 +0800

    added comments

--- js/booking.js
+++ js/booking.js
@@ -1,3 +1,4 @@
+// get data from the selection input
 $(function() {
     $ ( '#ddlselect' ).change(function()  {
         var displaycourse= $("#ddlselect option:selected").text();
@@ -5,6 +6,7 @@ $(function() {
     } ) 
 });
 
+// get data from the selection input
 $(function() {
     $ ( '#ddlselect1' ).change(function()  {
         var displaycourse= $("#ddlselect1 option:selected").text();
@@ -12,6 +14,7 @@ $(function() {
     } ) 
 });
 
+// get data from the selection input
 $(function() {
     $ ( '#ddlselect2' ).change(function()  {
         var displaycourse= $("#ddlselect2 option:selected").text();
@@ -19,6 +22,7 @@ $(function() {
     } ) 
 });
 
+// get data from the selection input
 $(function() {
     $ ( '#workPosition' ).change(function()  {
         var displaycourse= $("#workPosition option:selected").text();
@@ -26,6 +30,7 @@ $(function() {
     } ) 
 });
 
+// datepicker format on their input
 $(function() {
     $ ( '#datepicker' ) . datepicker ( {
         format: 'yyyy-mm-dd'
@@ -33,29 +38,35 @@ $(function() {
 });
 
 $(function() {
+    // datepicker format on their input
     $ ( '#datepicker2' ) . datepicker ( {
-        format: 'yyyy-mm-dd',
-    } ).on("changeDate", function (e){
-        var start= $("#datepicker").datepicker("getDate");
-        var end= $("#datepicker2").datepicker("getDate");
-        days = (end- start) / (1000 * 60 * 60 * 24);
-        document.getElementById("ans").innerHTML = days;
-
-        if(days > 0){
-            if (days >29) {
-                alert('Days selected more than 29 days, please select duration that are less than 29 days.');
-                document.getElementById("Button").disabled = true;
-            }else{
-                document.getElementById("Button").disabled = false;
+            format: 'yyyy-mm-dd',
+        } ).on("changeDate", function (e){
+            // get the variable from the date picker input 
+            var start= $("#datepicker").datepicker("getDate");
+            var end= $("#datepicker2").datepicker("getDate");
     
+            // calculate days differences based on the end and start date
+            days = (end- start) / (1000 * 60 * 60 * 24);
+            document.getElementById("ans").innerHTML = days;
+    
+            if(days > 0){
+                // if days more than 29 days (a month)
+                if (days >29) {
+                    alert('Days selected more than 29 days, please select duration that are less than 29 days.');
+                    document.getElementById("Button").disabled = true;
+                }else{
+                    // allow user submit
+                    document.getElementById("Button").disabled = false;
+        
+                }
+            }else{
+                // if days less than 1 day, disable user submit
+                alert('Days selected less than 1 day, please select duration that are more than 1 day.');
+                document.getElementById("Button").disabled = true;
             }
-        }else{
-            alert('Days selected less than 1 day, please select duration that are more than 1 day.');
-            document.getElementById("Button").disabled = true;
-        }
-    }) ;
-});
-
+        }) ;
+    });
 
 
 
--- js/check-in.js
+++ js/check-in.js
@@ -1,27 +1,36 @@
+// datepicker format on their input
 $(function() {
     $ ( '#datepicker' ) . datepicker ( {
         format: 'yyyy-mm-dd'
     } ) ;
 });
 
+
 $(function() {
-    $ ( '#datepicker2' ) . datepicker ( {
+// datepicker format on their input
+$ ( '#datepicker2' ) . datepicker ( {
         format: 'yyyy-mm-dd',
     } ).on("changeDate", function (e){
+        // get the variable from the date picker input 
         var start= $("#datepicker").datepicker("getDate");
         var end= $("#datepicker2").datepicker("getDate");
+
+        // calculate days differences based on the end and start date
         days = (end- start) / (1000 * 60 * 60 * 24);
         document.getElementById("ans").innerHTML = days;
 
         if(days > 0){
+            // if days more than 29 days (a month)
             if (days >29) {
                 alert('Days selected more than 29 days, please select duration that are less than 29 days.');
                 document.getElementById("Button").disabled = true;
             }else{
+                // allow user submit
                 document.getElementById("Button").disabled = false;
     
             }
         }else{
+            // if days less than 1 day, disable user submit
             alert('Days selected less than 1 day, please select duration that are more than 1 day.');
             document.getElementById("Button").disabled = true;
         }
@@ -29,7 +38,7 @@ $(function() {
 });
 
 $(document).ready(function(){
-
+// enable only one check for the room (cant select multiple rooms)
     $('.checkoption').click(function() {
        $('.checkoption').not(this).prop('checked', false);
     });
